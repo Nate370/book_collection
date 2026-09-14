b9b951d7f879ca6cfeaa5796a28b527d83e6aea5
Nate370 <shipman.nathaniel0405@gmail.com>
2026-09-13 23:15:53 -0500

    Revert "Book Collection 2 Complete (dev -> test)"

--- app/controllers/update_books_controller.rb
+++ app/controllers/update_books_controller.rb
@@ -1,27 +1,17 @@
 class UpdateBooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
-    flash.keep
   end
 
   def update
     @book = Book.find(params[:id])
-    issue = false
-    if (book_params[:title] == '' or book_params[:author] == '' or
-      book_params[:price] == '' or book_params[:published_date] == '')
-      issue = true
-      flash[:empty] = "All Book attributes must be filled in"
-    end
-    if (book_params[:price] != '' and book_params[:price].to_f < 0)
-      issue = true
-      flash[:negative] = "Book price must not be negative"
-    end
-    if (issue)
-      redirect_to new_update_book_path; return
+    if book_params[:title] == ''
+      flash[:notice] = "Book must have a title"
+      redirect_to edit_update_book_path; return
     end
     if @book.update(book_params)
       flash[:notice]= "Book updated successfully"
-      redirect_to root_path
+      redirect_to root_path #show_detail_path(@book)
     else
       render('edit')
     end
@@ -31,10 +21,7 @@ class UpdateBooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(
-      :title,
-      :author,
-      :price,
-      :published_date
+      :title
     )
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,2 @@
 class Book < ApplicationRecord
-  validates :title, :author, :price, :published_date, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 0}
 end
