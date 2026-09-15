c7387c19610403ce82a8e576fdd25e79ba2c09fb
Nate370 <shipman.nathaniel0405@gmail.com>
2026-09-14 23:30:21 -0500

    database seeding (again)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,17 @@
-User.create!([
-  {username: "Turtle person"},
-  {username: "Axolotl"}
-])
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+
 Book.create!([
-  {title: "Fall 2027", author: "Nathaniel", price: "0.0", published_date: "2027-08-23"},
-  {title: "Fall 2026", author: nil, price: nil, published_date: nil},
-  {title: "Fall 2027", author: nil, price: nil, published_date: nil}
-])
-UserBook.create!([
-  {user_id: 2, book_id: 2}
+    {title: "The life of a turtle", author: "greg", price: 90, published_date: Date.new(2026, 9, 14)},
+    {title: "The mirror, the lion, and the audacity of the little dog", author: "greg", price: 90, published_date: Date.new(2026, 9, 14)},
+    {title: "Origami for cats", author: "greg", price: 90, published_date: Date.new(2026, 9, 14)},
+    {title: "The weight of a hundred kilograms", author: "greg", price: 90, published_date: Date.new(2026, 9, 14)}
+    {title: "End of the world", author: "greg", price: 90, published_date: Date.new(2026, 9, 14)}
 ])
